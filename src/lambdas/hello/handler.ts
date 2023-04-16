be88292e6e29187a2eb4ac8482b6339bea258078
Kyle <davidson.j.kyle@gmail.com>
2023-04-16 21:40:38 +1200

    runs prettier over the boilerplate

--- src/lambdas/hello/handler.ts
+++ src/lambdas/hello/handler.ts
@@ -1,10 +1,11 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@utils/api-gateway';
-import { formatJSONResponse } from '@utils/api-gateway';
-import { middyfy } from '@utils/lambda';
+import type { ValidatedEventAPIGatewayProxyEvent } from "@utils/api-gateway";
+import { formatJSONResponse } from "@utils/api-gateway";
+import { middyfy } from "@utils/lambda";
+import schema from "./schema";
 
-import schema from './schema';
-
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) => {
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
